2a980a6bd8e2063c7e74d6f3564e747d146c0cb8
Rodrigo Castro <rodrigo.castro@avira.com>
2021-10-12 18:18:15 +0200

    finish setup step 3

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,12 @@
-const custom = require('../webpack.config');
+const custom = require('../webpack.config')
 
 module.exports = {
   stories: ['../src/components/**/*.stories.[tj]s'],
-  webpackFinal: config => {
+  webpackFinal: (config) => {
     return {
       ...config,
-      resolve: {alias: {...config.resolve.alias, ...custom.resolve.alias}},
-      module: {...config.module, rules: custom.module.rules},
-    };
+      resolve: { alias: { ...config.resolve.alias, ...custom.resolve.alias } },
+      module: { ...config.module, rules: custom.module.rules },
+    }
   },
-};
+}
